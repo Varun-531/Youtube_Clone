a4a08f638c38f630c260230ec90272f3a3fc36b4
Varun-531 <21pa1a0531@vishnu.edu.in>
2024-03-24 09:53:28 +0530

    Refactor App component and update CSS styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VedioDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/vedio/:id" element={<VedioDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  </div>
+);
 
-export default App
+export default App;
---
+++ src/components/Feed.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import { Box, Typography, Stack } from "@mui/material";
+
+const Feed = () => {
+  return <div>Feed</div>;
+};
+
+export default Feed;
